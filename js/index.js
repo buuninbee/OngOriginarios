38006627efdf29b8d546254e21fce9fce05e5d5e
Bruno <buunin.bee@gmail.com>
2023-08-26 17:27:39 -0300

    adicionando responsividade e atualizando as pages

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-function menushow() {
-    let menumobile = document.querySelector(".mobile-menu");
-    if (menumobile.classList.contains("open")) {
-      menumobile.classList.remove("open");
-      document.querySelector(".icon").src = "./img/icones/icone-closer.svg";
-    } else {
-      menumobile.classList.add("open");
-      document.querySelector(".icon").src = "./img/icones/icone-open.svg";
-    }
+const btnmenu = $("#menushow");
+
+btnmenu.on("click", () => {
+  let menumobile = document.querySelector(".mobile-menu");
+  if (menumobile.classList.contains("open")) {
+    menumobile.classList.remove("open");
+    document.querySelector(".icon").src = "./img/icones/icone-closer.svg";
+  } else {
+    menumobile.classList.add("open");
+    document.querySelector(".icon").src = "./img/icones/icone-open.svg";
   }
+});
