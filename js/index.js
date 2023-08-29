cf83950331972994b8d04e6b486fcab138acc53b
bruno <buunin.bee@gmail.com>
2023-08-29 11:59:31 -0300

    adicionando o menu responsivo

--- js/index.js
+++ js/index.js
@@ -2,11 +2,15 @@ const btnmenu = $("#menushow");
 
 btnmenu.on("click", () => {
   let menumobile = document.querySelector(".mobile-menu");
+
   if (menumobile.classList.contains("open")) {
     menumobile.classList.remove("open");
     document.querySelector(".icon").src = "./img/icones/icone-closer.svg";
+    menumobile.style.display = "grid";
   } else {
     menumobile.classList.add("open");
     document.querySelector(".icon").src = "./img/icones/icone-open.svg";
+    menumobile.style.display = "none";
+    menumobile.classList.add("open");
   }
 });
